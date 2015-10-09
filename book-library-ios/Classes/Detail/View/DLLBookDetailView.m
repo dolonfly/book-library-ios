c22834025664b1152f2d9b612f1ade4ad2dada84
ll.d <dolonfly@users.noreply.github.com>
2015-10-09 19:01:25 +0800

    add putInStorage button and like button

--- book-library-ios/Classes/Detail/View/DLLBookDetailView.m
+++ book-library-ios/Classes/Detail/View/DLLBookDetailView.m
@@ -17,8 +17,8 @@
 @property (nonatomic, weak) UILabel *nameLabel;
 @property (nonatomic, weak) UILabel *authorLabel;
 @property (nonatomic, weak) UILabel *publisherLabel;
-@property (nonatomic, weak) UILabel *borrowLabel;
-@property (nonatomic, weak) UILabel *putInStorageLabel;
+@property (nonatomic, weak) UIButton *putInStorageBtn;
+@property (nonatomic, weak) UIButton *likeBtn;
 
 @end
 
@@ -55,13 +55,17 @@
         [self addSubview:publisherLabel];
         self.publisherLabel = publisherLabel;
         
-        UILabel *borrowLabel = [[UILabel alloc] init];
-        [self addSubview:borrowLabel];
-        self.borrowLabel = borrowLabel;
+        UIButton *putInStoreageBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        putInStoreageBtn.tag = 0;
+        [putInStoreageBtn setTitle:@"入库" forState:UIControlStateNormal];
+        [self addSubview:putInStoreageBtn];
+        self.putInStorageBtn = putInStoreageBtn;
         
-        UILabel *putInStorageLabel = [[UILabel alloc] init];
-        [self addSubview:putInStorageLabel];
-        self.putInStorageLabel = putInStorageLabel;
+        UIButton *likeBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        likeBtn.tag = 1;
+        [likeBtn setTitle:@"推荐" forState:UIControlStateNormal];
+        [self addSubview:likeBtn];
+        self.likeBtn = likeBtn;
 
     }
     return self;
@@ -71,7 +75,7 @@
 - (void)layoutSubviews
 {
     [super layoutSubviews];
-    self.backgroundColor = [UIColor yellowColor];
+    self.backgroundColor = [UIColor colorWithRed:255 green:255 blue:255 alpha:50];
    
     
     float y = 200;
@@ -79,18 +83,45 @@
     float fatherFrameWidth = self.frame.size.width;
     // set frame
     
-    
-    
-    
     y = divsion + self.imgView.frame.origin.y + self.imgView.frame.size.height;
     
     self.nameLabel.frame = CGRectMake(0, y, fatherFrameWidth, 20);
     self.nameLabel.contentMode = UIViewContentModeCenter;
     [self.nameLabel setTextAlignment:NSTextAlignmentCenter];
     
+    y = divsion + self.nameLabel.frame.origin.y + self.nameLabel.frame.size.height;
+    self.authorLabel.frame = CGRectMake(0, y, fatherFrameWidth, 20);
+    self.authorLabel.contentMode = UIViewContentModeCenter;
+    [self.authorLabel setTextAlignment:NSTextAlignmentCenter];
+    self.authorLabel.textColor = [UIColor grayColor];
+
     
-    NSLog(@"image frame yyyy:%f",self.imgView.image.size.height);
-    NSLog(@"image view h:%f",y);
+    y =  self.authorLabel.frame.origin.y + self.authorLabel.frame.size.height;
+    self.publisherLabel.frame = CGRectMake(0, y, fatherFrameWidth, 20);
+    self.publisherLabel.contentMode = UIViewContentModeCenter;
+    [self.publisherLabel setTextAlignment:NSTextAlignmentCenter];
+    self.publisherLabel.textColor = [UIColor grayColor];
+    
+    self.nameLabel.font = [UIFont systemFontOfSize:18 weight:18];
+    
+    
+    //button
+    y = divsion + self.publisherLabel.frame.origin.y + self.publisherLabel.frame.size.height;
+    self.putInStorageBtn.frame = CGRectMake(fatherFrameWidth / 2 - 60 - 10, y, 60, 40);
+//    self.putInStorageBtn.titleLabel.font = [UIFont systemFontOfSize:18 weight:10];
+    [self.putInStorageBtn setTitleShadowColor:[UIColor grayColor] forState:UIControlStateNormal];
+    self.putInStorageBtn.layer.cornerRadius = 5.0f;
+    self.putInStorageBtn.layer.masksToBounds = YES;
+    self.putInStorageBtn.layer.borderWidth = 0.5f;
+    self.putInStorageBtn.layer.borderColor = [[UIColor grayColor] CGColor];
+    
+    // 推荐按钮
+    self.likeBtn.frame = CGRectMake(fatherFrameWidth/2+10, y, 60, 40);
+    [self.likeBtn setTitleShadowColor:[UIColor grayColor] forState:UIControlStateNormal];
+    self.likeBtn.layer.cornerRadius = 5.0f;
+    self.likeBtn.layer.masksToBounds = YES;
+    self.likeBtn.layer.borderWidth = 0.5f;
+    self.likeBtn.layer.borderColor = [[UIColor grayColor] CGColor];
     
 }
 
@@ -121,6 +152,13 @@
             //    temp.origin.y = y;
             // 3. 修改frame的值
             self.imgView.frame = temp;
+            
+            self.imgView.layer.borderWidth = 1;
+            self.imgView.layer.borderColor = [UIColor colorWithRed:218 green:218 blue:218 alpha:0.2].CGColor;
+            self.imgView.contentMode = UIViewContentModeScaleAspectFill;
+
+            
+            
         }else{
             NSLog(@"download image err");
         }
@@ -128,8 +166,9 @@
 //    [self.imgView sd_setImageWithURL:[NSURL URLWithString:dllBook.bookImage] placeholderImage:nil];
     
     self.nameLabel.text = dllBook.bookName;
-    self.authorLabel.text = [dllBook.authors componentsJoinedByString:@","];
-    self.publisherLabel.text = dllBook.publisher;
+    self.authorLabel.text = [NSString stringWithFormat:@"作者：%@",[dllBook.authors componentsJoinedByString:@","]];
+    self.publisherLabel.text = [NSString stringWithFormat:@"出版社：%@",dllBook.publisher];
+    NSLog(@"出版社：%@",dllBook.publisher);
     
 }
 
